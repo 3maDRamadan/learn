61b4e3e3d741f723d996ca774ac5f1695a3e1dfb
Emad Ramadan <madr5953@gmail.com>
2023-08-16 12:47:55 +0300

    lesson-6

---
+++ src/comp/foot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Foot = () => {
+  return (
+      <footer>
+        Designed and developed by Courses4Arab.com
+        <span>🧡</span>
+      </footer>
+  );
+}
+
+export default Foot;
