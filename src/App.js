0171d48706f2400d65204c4931500748291296bd
Emad Ramadan <madr5953@gmail.com>
2023-08-16 10:07:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h5>new branch </h5>
+      <h1 className="title">c4a.dev</h1>
+      <h2>Emad Ramadan</h2>
+      <p>Hello world</p>
+    </>
+  );
+}
+
+export default App;
